88c75d3deec4204233f2c1bdad622db7f2811f82
michaelmeidenbauer <70448346+michaelmeidenbauer@users.noreply.github.com>
2023-08-08 18:04:18 -0700

    teaching

--- teaching.php
+++ teaching.php
@@ -24,7 +24,7 @@
                         <br />
                         <strong>Psychology 312:</strong> Experimental Methods in Psychology
                         <br />
-                        <strong>Psychology 333:</strong> Abnormal Psychology
+                        <strong>Psychology 333:</strong> Fundamentals of Psychological Disorders
                         <br />
                         <strong>Psychology 361:</strong> Fundamentals of Developmental Psychology
                         <br />
@@ -37,8 +37,9 @@
                     </div>
                     </br>
                     <h2> Open Educational Resources</h2>
-                    <p class="c31"><span class="c3 c18">Jhangiani, R. S., Chiang, I. A., </span><span class="c6 c18">Cuttler, C. </span><span class="c3 c18">, &amp; Leighton, D. C.,(2019). Research methods in psychology: 4th edition. </span><span class="c3 c18">&nbsp;Pullman, WA: Open Text Kwantlen Polytechnic University. Retrieve at: </span><span class="c10 c3 c8"><a class="c23" href="https://kpu.pressbooks.pub/psychmethods4e/">https://kpu.pressbooks.pub/psychmethods4e/</a></span></p>
-                    <p class="c31"><span class="c3 c18">Price, P. C., Jhangiani, R. S., Chiang, I. A., Leighton, D. C., &amp; </span><span class="c6 c18">Cuttler, C. </span><span class="c3 c18">(2017). Research methods in psychology: 3</span><span class="c20 c18 c25">rd</span><span class="c3 c18">&nbsp;American edition. Pullman, WA: Open Text WSU. Retrieve at: https:</span><span class="c10 c3 c8"><a class="c23" href="https://opentext.wsu.edu/carriecuttler/">opentext.wsu.edu/carriecuttler/</a></span></p>
+                    <p class="c31"><span class="c3 c18">Bridley, A, Daffin, L. W. Jr., &amp; <span class="c6 c18">Cuttler, C.</span> (2022). Fundamentals of psychological disorders. Pullman, WA: OpenText WSU. </span><span class="c10 c3 c8"><a class="c23" href="https://opentext.wsu.edu/fundamentalsofpsychologicaldisorders/">https://opentext.wsu.edu/fundamentalsofpsychologicaldisorders/</a></span></p>
+                    <p class="c31"><span class="c3 c18">Jhangiani, R. S., Chiang, I. A., </span><span class="c6 c18">Cuttler, C. </span><span class="c3 c18">, &amp; Leighton, D. C.,(2019). Research methods in psychology: 4th edition. </span><span class="c3 c18"> Vancouver, BC: Open Text Kwantlen Polytechnic University. Peer Reviewed.</span><span class="c10 c3 c8"><a class="c23" href="https://kpu.pressbooks.pub/psychmethods4e/">https://kpu.pressbooks.pub/psychmethods4e/</a></span></p>
+                    <p class="c31"><span class="c3 c18">Price, P. C., Jhangiani, R. S., Chiang, I. A., Leighton, D. C., &amp; </span><span class="c6 c18">Cuttler, C. </span><span class="c3 c18">(2017). Research methods in psychology: 3</span><span class="c20 c18 c25">rd</span><span class="c3 c18">&nbsp;American edition. Pullman, WA: Open Text WSU. Retrieve at: </span><span class="c10 c3 c8"><a class="c23" href="https://opentext.wsu.edu/carriecuttler/">opentext.wsu.edu/carriecuttler/</a></span></p>
                     <p class="c31"><span class="c3 c18">Bridley, A. &amp; Daffin, L. W. Jr. (2018). Abnormal Psychology: 1</span><span class="c20 c18 c25">st</span><span class="c3 c18">&nbsp;edition. </span><span class="c6 c18">C. Cuttler</span><span class="c3 c18">&nbsp;(Ed.). Pullman, WA: Open Text WSU. Retrieve at: </span><span class="c10 c3 c8"><a class="c23" href="https://opentext.wsu.edu/abnormalpsychology/">https://opentext.wsu.edu/abnormalpsychology/</a></span></p>
 
                 </article>
